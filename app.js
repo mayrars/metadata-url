02a24b3e20e4f4b58329528bfd7fd9adbf4c6ed9
Mayra <mayra203@gmail.com>
2018-05-02 12:11:05 -0500

    Insert element in DB

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const express = require('express')
 const request = require('request')
 var bodyParser = require('body-parser')
+var admin = require("firebase-admin");
 const urlMetadata = require('url-metadata')
 const port = process.env.PORT || 3000
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-app.post('/',urlencodedParser,(req, res)=>{
-	var data =  req.body.urlpage
+var serviceAccount = require("./metadata-url-firebase-adminsdk-j7sdi-58dc93e595.json");
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://metadata-url.firebaseio.com"
+});
+var defaultDatabase = admin.database()
+var ref = defaultDatabase.ref('/')
+app.post('/', urlencodedParser, (req, res) => {
+	var data = req.body.urlpage
 	urlMetadata(data).then(
-	function (metadata) { // success handler
-		res.end(JSON.stringify(metadata))
-	},
-	function (error) { // failure handler
-		res.end(JSON.stringify(error))
-	})
-	//res.end(JSON.stringify(data))
+		function (metadata) { // success handler
+			savemetadata(metadata)
+		},
+		function (error) { // failure handler
+			res.end(error)
+		})
 })
+
+function savemetadata(data) {
+    ref.push(data,()=>{
+		console.log('Insertado')
+	})
+}
 app.use('/', express.static(__dirname + '/public'))
 
 app.listen(port, () => {
